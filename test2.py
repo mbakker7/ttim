25ad221080b91538fcec5772ea5486a3320dda41
Mark Bakker <markbak@gmail.com>
2017-11-07 17:17:04 +0100

    working on testing

--- test2.py
+++ test2.py
@@ -1,13 +1,14 @@
 from ttim import *
+import numpy as np
 
-ml = ModelMaq(kaq = [1.0, 5.0],
-                   z = [3, 2, 1,0],
-                   c = [10.],
-                   Saq = [0.3, 0.01],
-                   Sll = [0.001],
-                   tmin = 1e-3,
-                   tmax = 1e6,
-                   M = 15)
-w1 = HeadWell(ml, xw = 0, yw = 0, rw = 1e-5, tsandh = [(0, 1)], layers = 0)
+ml = ModelMaq(kaq=[1, 5], z=[3, 2, 1, 0], c=[10], Saq=[0.3, 0.01], Sll=[0.001], tmin=10, tmax=1000, M=20)
+w1 = HeadWell(ml, xw=0, yw=0, rw=0.3, tsandh=[(0, 1)], layers=0)
 ml.solve()
 
+ml2 = ModelMaq(kaq=[1, 5], z=[3, 2, 1, 0], c=[10], Saq=[0.3, 0.01], Sll=[0.001], tmin=10, tmax=1000, M=20)
+w2 = DischargeWell(ml2, xw=0, yw=0, rw=0.3, tsandQ=[(0, 0), (100, 2.15)], layers=0)
+ml2.solve()
+
+x = np.linspace(-10, 10, 101)
+h1 = ml.headalongline(x, np.zeros_like(x), 110, [0, 1])
+h2 = ml2.headalongline(x, np.zeros_like(x), 110, [0, 1])
---
+++ test3.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.integrate import quad
+
+def func1(tau, p0, p1, f):
+    rv = np.exp(-tau) * np.cos(-p1 / p0 * tau) * f(tau / p0)
+    return rv
+
+def func2(tau, p0, p1, f):
+    rv = np.exp(-tau) * np.sin(-p1 / p0 * tau) * f(tau / p0)
+    return rv
+
+def func(t):
+    return np.exp(-t)
+
+p0 = 0.2
+p1 = 0.4
+
+def quadfunc():
+    f1 = quad(func1, 0, np.inf, args=(p0, p1, func))[0]
+    f2 = quad(func2, 0, np.inf, args=(p0, p1, func))[0]
+    return (f1 + 1j * f2) / p0
+
+f = quadfunc()
+
+
+def gausslag1(tau, p0, p1, f):
+    rv = np.cos(-p1 / p0 * tau) * f(tau / p0)
+    return rv
+
+def gausslag2(tau, p0, p1, f):
+    rv = np.sin(-p1 / p0 * tau) * f(tau / p0)
+    return rv
+
+def lag():
+    x, w = np.polynomial.laguerre.laggauss(50)
+    g1 = 0.0
+    g2 = 0.0
+    for i in range(len(x)):
+        g1 += w[i] * gausslag1(x[i], p0, p1, func)
+        g2 += w[i] * gausslag2(x[i], p0, p1, func)
+    return (g1 + 1j * g2) / p0
+    
+g = lag()
+
