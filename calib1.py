309f73245311d6eecc3ab4e42d641290b93988e1
Mark Bakker <markbak@gmail.com>
2017-12-23 15:28:14 +0100

    removed some old notebooks

---
+++ calib1.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import Parameters, minimize, fit_report
+from ttim import *
+
+observed = np.loadtxt('oudekorendijk_h30.dat')
+to1 = observed[:,0] / 60.0 / 24.0
+ho1 = -observed[:,1]
+ro1 = 30.0
+Qo = 788.0
+ml = ModelMaq(kaq=60, z=(-18,-25), Saq=1e-4, tmin=1e-5, tmax=1)
+w = Well(ml, xw=0, yw=0, rw=0.1, tsandQ=[(0, 788)], layers=0)
+ml.solve(silent='.')
+
+#################
+import numpy as np
+from lmfit import Parameters, minimize, fit_report
+
+class Fit:
+    def __init__(self, model):
+        self.model = model
+        self.lmfitparams = Parameters()
+        self.parameterdict = {}
+        self.seriesdict = {}
+    def parameter(self, name, par=None, initial=0, vmin=None, vmax=None, vary=True):
+        if par is not None:
+            if type(par) != np.ndarray:
+                p = np.array(par)
+            else:
+                p = par
+        else:
+            if name[:3] == 'kaq':
+                layer = int(name[3:])
+                p = self.model.aq.kaq[layer:layer + 1]
+            elif name[:3] == 'Saq':
+                layer = int(name[3:])
+                p = self.model.aq.Saq[layer:layer + 1]
+            else:
+                print('parameter name not recognized or no par reference supplied')
+                return
+        self.lmfitparams.add(name, value=initial, min=vmin, max=vmax, vary=vary)
+        self.parameterdict[name] = p
+    def series(self, name, x, y, t, h):
+        s = Series(x, y, t, h)
+        self.seriesdict['name'] = s
+        
+class Series:
+    def __init__(self, x, y, t, h):
+        self.x = x
+        self.y = y
+        self.t = t
+        self.h = h
+        
+#####################
+
+fit = Fit(ml)
+fit.parameter('kaq0', initial=10)
+fit.parameter('Saq0', initial=1e-4)
+fit.series('obs1', ro1, 0, to1, ho1)
+
+
+#def residual(p, t=None, rdata=None, data=None):
+#    vals = p.valuesdict()
+#    k =  vals['k']
+#    Ss =  vals['Ss']
+#    ml.aq.set_kaq(k, 0)
+#    ml.aq.set_Saq(Ss, 0)
+#    ml.solve(silent='.')
+#    hm = ml.head(rdata, 0.0, t, layers=[0])
+#    if data is None:
+#        return hm[0]
+#    else:
+#        return hm[0] - data  # head in layer 0
+#    
+#p = Parameters()
+#p.add('k', value=10.0, min=5)
+#p.add('Ss', value=1e-4)
+#
+#print('Parameter estimation using data at observation well 30 m')
+#p30 = minimize(residual, p, kws={'t':to1, 'rdata':ro1, 'data':ho1}, epsfcn=1e-4)
+#print()
+#print(fit_report(p30.params))
+
+
