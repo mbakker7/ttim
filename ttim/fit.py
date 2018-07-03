91c49871a5ae3ecf3c29b336c48848f51837a5ad
Mark Bakker <markbak@gmail.com>
2018-07-03 22:13:54 +0200

    changed fit so that least_squares is default
    Default nonlinear solver is scipy.optimize.least_squares.
    Lmfit is still available with CalibrateOld

--- ttim/fit.py
+++ ttim/fit.py
@@ -1,8 +1,77 @@
 import numpy as np
-from lmfit import Parameters, minimize, fit_report
+import pandas as pd
+from scipy.optimize import least_squares
 
 class Calibrate:
     def __init__(self, model):
+        self.model = model
+        self.parameters = pd.DataFrame(columns=['initial', 'optimal', 'pmin', 'pmax'])
+        self.seriesdict = {}
+    def set_parameter(self, name=None, parameter=None, layer=0, initial=0, pmin=None, pmax=None):
+        """
+        if name is 'kaq#' or 'Saq#', no parameter of layer needs to be provided
+        otherwise, parameter needs to be an array and layer needs to be specified
+        """
+        if parameter is not None:
+            assert isinstance(parameter, np.ndarray), "Error: parameter needs to be numpy array"
+            p = par[layer:layer + 1]
+        if isinstance(name, str):
+            # Set, kaq, Saq
+            if name[:3] == 'kaq':
+                layer = int(name[3:])
+                p = self.model.aq.kaq[layer:layer + 1]
+            elif name[:3] == 'Saq':
+                layer = int(name[3:])
+                p = self.model.aq.Saq[layer:layer + 1]
+            elif name[0] == 'c':
+                layer = int(name[1:])
+                p = self.model.aq.c[layer:layer + 1]
+            # TO DO: set c, Sll
+            else:
+                print('parameter name not recognized or no parameter reference supplied')
+                return
+        else:
+            assert isinstance(parameter, np.ndarray), "Error: parameter needs to be array"
+        self.parameters.loc[name] = {'initial':initial, 'pmin':pmin, 'pmax':pmax, 'optimal':p[:]}
+        #self.parametersdict[name] = p
+    def series(self, name, x, y, layer, t, h):
+        s = Series(x, y, layer, t, h)
+        self.seriesdict[name] = s
+    def residuals(self, p):
+        print('.', end='')
+        # set the values of the variables
+        for i, k in enumerate(self.parameters.index):
+            self.parameters.loc[k, 'optimal'][:] = p[i]  # [:] needed to do set value in array
+        self.model.solve(silent=True)
+        rv = np.empty(0)
+        for key in self.seriesdict:
+            s = self.seriesdict[key]
+            h = self.model.head(s.x, s.y, s.t, layers=s.layer)
+            rv = np.append(rv, s.h - h)
+        return rv
+    def fit(self, report=True):
+        self.fitresult = least_squares(self.residuals, self.parameters.initial.values,
+                         method='lm', diff_step=1e-4, xtol=1e-4)
+        # Call residuals to specify optimal values for model
+        res = self.residuals(self.fitresult.x)
+        print('')
+        nparam = len(self.fitresult.x)
+        H = self.fitresult.jac.T @ self.fitresult.jac
+        sigsq = np.var(res, ddof=nparam)
+        self.covmat = np.linalg.inv(H) * sigsq 
+        self.sig = np.sqrt(np.diag(self.covmat))
+        D = np.diag(1 / self.sig)
+        self.cormat = D @ self.covmat @ D
+        if report:
+            print(self.parameters)
+            print(self.sig)
+            print(self.covmat)
+            print(self.cormat)
+            
+            
+class CalibrateOld:
+    def __init__(self, model):
+        from lmfit import Parameters, minimize, fit_report
         self.model = model
         self.lmfitparams = Parameters()
         self.parameterdict = {}
