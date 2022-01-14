b60a35a661689464c8138139297cac18f15f9541
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2022-01-14 20:55:37 +0100

    move to github actions

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+
 version = {}
 with open("ttim/version.py") as fp:
     exec(fp.read(), version)
@@ -17,12 +18,14 @@ setup(
     version=version["__version__"],
     description="Transient multi-layer AEM Model",
     long_description=l_d,
+    long_description_content_type='text/markdown',
     author="Mark Bakker",
     author_email="markbak@gmail.com",
     url="https://github.com/mbakker7/ttim",
     license="MIT",
     packages=["ttim"],
     python_requires='>=3.7',
-    install_requires=["numpy>=1.17", "scipy>=1.5", "numba>=0.5", "matplotlib>=3.1", "lmfit>=1.0", "pandas>=1.1"],
+    install_requires=["numpy>=1.17", "scipy>=1.5", "numba>=0.5",
+                      "matplotlib>=3.1", "lmfit>=1.0", "pandas>=1.1"],
     classifiers=['Topic :: Scientific/Engineering :: Hydrology'],
 )
