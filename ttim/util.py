f0464866bbf177c598e17767c72f97afb7f797c0
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-11-06 13:17:04 +0100

    Make PlotTim a submodule of the Model class. - add topview method (the old .plot() - add xsection() - add head_along_line(), the old xsection - move to object oriented plotting using ax instead of plt

--- ttim/util.py
+++ ttim/util.py
@@ -1,31 +1,120 @@
+from typing import Optional
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 class PlotTtim:
-    def plot(self, win=None, newfig=True, figsize=None):
-        """Plot layout.
+    def __init__(self, ml):
+        self._ml = ml
+
+    def topview(self, win=None, ax=None, figsize=None, layers=None):
+        """Plot top-view.
 
         Parameters
         ----------
         win : list or tuple
             [x1, x2, y1, y2]
         """
-        if newfig:
-            plt.figure(figsize=figsize)
-            ax1 = plt.subplot()
-        else:
-            fig = plt.gcf()
-            ax1 = fig.axes[0]
-        if ax1 is not None:
-            plt.sca(ax1)
-            for e in self.elementlist:
-                e.plot()
-            plt.axis("scaled")
-            if win is not None:
-                plt.axis(win)
+        if ax is None:
+            _, ax = plt.subplots(figsize=figsize)
+        if layers is not None and isinstance(layers, int):
+            layers = [layers]
+        for e in self._ml.elementlist:
+            if layers is None or np.isin(e.layers, layers):
+                e.plot(ax=ax)
+        if win is not None:
+            ax.axis(win)
+            ax.axis("scaled")
+        return ax
 
     def xsection(
+        self,
+        xy: Optional[list[tuple[float]]] = None,
+        labels=True,
+        params=False,
+        ax=None,
+    ):
+        if ax is None:
+            _, ax = plt.subplots(1, 1, figsize=(8, 4))
+
+        if xy is not None:
+            (x0, y0), (x1, y1) = xy
+            r = np.sqrt((x1 - x0) ** 2 + (y1 - y0) ** 2)
+            ax.set_xlim(0, r)
+        else:
+            r = 1.0
+
+        if labels:
+            lli = 1 if self._ml.aq.topboundary == "con" else 0
+            aqi = 0
+
+        for i in range(self._ml.aq.nlayers):
+            if self._ml.aq.ltype[i] == "l":
+                ax.axhspan(
+                    ymin=self._ml.aq.z[i + 1],
+                    ymax=self._ml.aq.z[i],
+                    color=[0.8, 0.8, 0.8],
+                )
+                if labels:
+                    ax.text(
+                        0.5 * r if not params else 0.25 * r,
+                        np.mean(self._ml.aq.z[i : i + 2]),
+                        f"leaky layer {lli}",
+                        ha="center",
+                        va="center",
+                    )
+                if params:
+                    ax.text(
+                        0.75 * r if labels else 0.5 * r,
+                        np.mean(self._ml.aq.z[i : i + 2]),
+                        f"$c$ = {self._ml.aq.c[lli]}, $S_s$ = {self._ml.aq.Sll[lli]:.2e}",
+                        ha="center",
+                        va="center",
+                    )
+                if labels or params:
+                    lli += 1
+
+            if labels and self._ml.aq.ltype[i] == "a":
+                ax.text(
+                    0.5 * r if not params else 0.25 * r,
+                    np.mean(self._ml.aq.z[i : i + 2]),
+                    f"aquifer {aqi}",
+                    ha="center",
+                    va="center",
+                )
+            if params and self._ml.aq.ltype[i] == "a":
+                if aqi == 0 and self._ml.aq.phreatictop:
+                    paramtxt = (
+                        f"$k_h$ = {self._ml.aq.kaq[aqi]}, "
+                        f"$S$ = {self._ml.aq.Saq[aqi]}"
+                    )
+                else:
+                    paramtxt = (
+                        f"$k_h$ = {self._ml.aq.kaq[aqi]}, "
+                        f"$S_s$ = {self._ml.aq.Saq[aqi]:.2e}"
+                    )
+                ax.text(
+                    0.75 * r if labels else 0.5 * r,
+                    np.mean(self._ml.aq.z[i : i + 2]),
+                    paramtxt,
+                    ha="center",
+                    va="center",
+                )
+            if (labels or params) and self._ml.aq.ltype[i] == "a":
+                aqi += 1
+
+        for i in range(1, self._ml.aq.nlayers):
+            if self._ml.aq.ltype[i] == "a" and self._ml.aq.ltype[i - 1] == "a":
+                ax.axhspan(
+                    ymin=self._ml.aq.z[i], ymax=self._ml.aq.z[i], color=[0.8, 0.8, 0.8]
+                )
+
+        ax.axhline(self._ml.aq.z[0], color="k", lw=0.75)
+        ax.axhline(self._ml.aq.z[-1], color="k", lw=3.0)
+        ax.set_ylabel("elevation")
+        return ax
+
+    def head_along_line(
         self,
         x1=0,
         x2=1,
@@ -38,27 +127,27 @@ class PlotTtim:
         color=None,
         lw=1,
         figsize=None,
-        newfig=True,
+        ax=None,
         legend=True,
     ):
         layers = np.atleast_1d(layers)
-        if newfig:
-            plt.figure(figsize=figsize)
+        if ax is None:
+            _, ax = plt.subplots(figsize=figsize)
         x = np.linspace(x1, x2, npoints)
         y = np.linspace(y1, y2, npoints)
         s = np.sqrt((x - x[0]) ** 2 + (y - y[0]) ** 2) + sstart
-        h = self.headalongline(x, y, t, layers)
+        h = self._ml.headalongline(x, y, t, layers)
         nlayers, ntime, npoints = h.shape
         for i in range(nlayers):
             for j in range(ntime):
                 if color is None:
-                    plt.plot(s, h[i, j, :], lw=lw)
+                    ax.plot(s, h[i, j, :], lw=lw)
                 else:
-                    plt.plot(s, h[i, j, :], color, lw=lw)
+                    ax.plot(s, h[i, j, :], color, lw=lw)
         if legend:
             legendlist = ["layer " + str(i) for i in layers]
-            plt.legend(legendlist)
-        # plt.draw()
+            ax.legend(legendlist, loc=(0, 1), ncol=3, frameon=False)
+        return ax
 
     def contour(
         self,
@@ -71,7 +160,7 @@ class PlotTtim:
         labels=False,
         decimals=0,
         color=None,
-        newfig=True,
+        ax=None,
         figsize=None,
         legend=True,
     ):
@@ -98,8 +187,8 @@ class PlotTtim:
             number of decimals of labels along contours
         color : str or list of strings
             color of contour lines
-        newfig : boolean (default True)
-            create new figure
+        ax : matplotlib.Axes
+            axes to plot on, default is None which creates a new figure
         figsize : tuple of 2 values (default is mpl default)
             size of figure
         legend : list or boolean (default True)
@@ -114,9 +203,10 @@ class PlotTtim:
         layers = np.atleast_1d(layers)
         xg = np.linspace(x1, x2, nx)
         yg = np.linspace(y1, y2, ny)
-        h = self.headgrid(xg, yg, t, layers)
-        if newfig:
-            plt.figure(figsize=figsize)
+        h = self._ml.headgrid(xg, yg, t, layers)
+        if ax is None:
+            _, ax = plt.subplots(figsize=figsize)
+            ax.axis("scaled")
         # color
         if color is None:
             c = plt.rcParams["axes.prop_cycle"].by_key()["color"]
@@ -125,25 +215,28 @@ class PlotTtim:
         elif isinstance(color, list):
             c = color
         if len(c) < len(layers):
-            n = np.ceil(self.aq.naq / len(c))
+            n = np.ceil(self._ml.aq.naq / len(c))
             c = n * c
 
         # contour
         cslist = []
         cshandlelist = []
         for i in range(len(layers)):
-            cs = plt.contour(xg, yg, h[i, 0], levels, colors=c[i])
+            cs = ax.contour(
+                xg, yg, h[i, 0], levels, colors=c[i], negative_linestyles="solid"
+            )
             cslist.append(cs)
             handles, labels = cs.legend_elements()
             cshandlelist.append(handles[0])
             if labels:
                 fmt = f"%1.{decimals}f"
-                plt.clabel(cs, fmt=fmt)
+                ax.clabel(cs, fmt=fmt)
         if isinstance(legend, list):
-            plt.legend(cshandlelist, legend)
+            ax.legend(cshandlelist, legend, loc=(0, 1), ncol=3, frameon=False)
         elif legend:
             legendlist = ["layer " + str(i) for i in layers]
-            plt.legend(cshandlelist, legendlist)
-        plt.axis("scaled")
+            ax.legend(cshandlelist, legendlist, loc=(0, 1), ncol=3, frameon=False)
+
         if layout:
-            self.plot(win=[x1, x2, y1, y2], newfig=False)
+            self.topview(win=[x1, x2, y1, y2], ax=ax)
+        return ax
