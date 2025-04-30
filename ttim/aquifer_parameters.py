45964128f19803d3b1026d6fd2374f4663863089
Mattijs Borst <m.borst@cwgi.nl>
2025-04-30 09:40:52 +0200

    Check z values

--- ttim/aquifer_parameters.py
+++ ttim/aquifer_parameters.py
@@ -26,6 +26,9 @@ def param_maq(
     )
     if topboundary[:3] == "con":
         naq = int(len(z) / 2)
+        assert len(z) == 2 * naq, (
+            "Error: Length of z needs to be 2 * number of aquifers for Maq models with confined top boundary."
+        )
         if len(kaq) == 1:
             kaq = kaq * np.ones(naq)
         if len(Saq) == 1:
@@ -65,6 +68,9 @@ def param_maq(
         ltype[1::2] = "l"
     else:  # leaky layers on top
         naq = int(len(z - 1) / 2)
+        assert len(z) == 2 * naq + 1, (
+            "Error: Length of z needs to be 2 * number of aquifers + 1 as top for Maq models with leaky top boundary."
+        )
         if len(kaq) == 1:
             kaq = kaq * np.ones(naq)
         if len(Saq) == 1:
